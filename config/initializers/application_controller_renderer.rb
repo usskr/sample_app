fb15961ccb778181c2bd753a4f1b55f04bbd0bb7
REI TAKAGI <rei.takagi731@gmail.com>
2021-07-29 04:05:21 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f3b2b7cae87f7b833729f3436303dbf804fc273524eef9727ac9c8e7420a97cbd280faae143db1dfebc4633ed487511273bbfbb6134239841cc1dcfae4ba0f8e'
