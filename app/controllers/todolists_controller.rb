fabbd3d3e93f7651d6094e94ec13ef641861a095
REI TAKAGI <rei.takagi731@gmail.com>
2021-07-29 01:50:12 +0000

    [Start] 2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -16,6 +16,9 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  def edit
+    @list = List.find(params[:id])
+  end
 
   private
   def list_params
